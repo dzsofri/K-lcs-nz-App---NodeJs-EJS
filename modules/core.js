d555638a48d5582936a52ca2f30c444b32862170
dzsofri <beretizsofia@turr.hu>
2024-11-06 09:54:18 +0100

    core.js

---
+++ modules/core.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const ejs = require('ejs');
+const axios = require('axios'); // Axios a statisztikák lekéréséhez
+const router = express.Router();
+
+// CORE routes
+
+// Kezdőlap betöltése
+router.get('/', (req, res) => {
+    ejs.renderFile('./views/index.ejs', { session: req.session }, (err, html) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        req.session.msg = '';
+        res.send(html);
+    });
+});
+
+// Regisztrációs oldal betöltése
+router.get('/reg', (req, res) => {
+    ejs.renderFile('./views/regist.ejs', { session: req.session }, (err, html) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        req.session.msg = '';
+        res.send(html);
+    });
+});
+
+/*
+// Új adat bevitele
+router.get('/newdata', (req, res) => {
+    if (req.session.isLoggedIn) {
+        ejs.renderFile('./views/newdata.ejs', { session: req.session }, (err, html) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            req.session.msg = '';
+            res.send(html);
+        });
+        return;
+    }
+    res.redirect('/');
+});*/
+
+
+/*
+// Kijelentkezés
+router.get('/logout', (req, res) => {
+    req.session.isLoggedIn = false;
+    req.session.userID = null;
+    req.session.userName = null;
+    req.session.userEmail = null;
+    req.session.userRole = null;
+    req.session.msg = 'You are logged out!';
+    req.session.severity = 'info';
+    res.redirect('/');
+});
+
+module.exports = router;
+*/
