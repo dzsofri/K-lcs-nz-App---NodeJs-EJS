74bce1d6ef93ffc95c04dfec52f59dc8e993a2d1
dzsofri <beretizsofia@turr.hu>
2024-11-11 10:00:49 +0100

    nagyon működö changes

--- modules/core.js
+++ modules/core.js
@@ -2,7 +2,6 @@ const express = require('express');
 const ejs = require('ejs');
 const db = require('./database');
 const router = express.Router();
-const db = require('./database');
 const moment = require('moment');
 
 // CORE routes
@@ -36,7 +35,7 @@ router.get('/kolcsonzo', (req, res) => {
     if (req.session.isLoggedIn) {
         db.query(`
             SELECT * FROM items
-            ORDER BY rentals.rental_date ASC
+          
         `,(err, results) => {
             if (err) {
  
@@ -77,57 +76,7 @@ router.get('/kolcsonzo', (req, res) => {
     res.redirect('/');
 });
 
-// Saját kölcsönzés lekérése
-router.get('/own', (req, res) => {
 
-    if (req.session.isLoggedIn) {
-
-        // SQL lekérdezés a saját kölcsönzésekhez, felhasználó nevével és tárgy nevével
-        db.query(`
-            SELECT rentals.*, users.name AS user_name, items.title AS item_title
-            FROM rentals
-            JOIN users ON rentals.user_id = users.user_id
-            JOIN items ON rentals.item_id = items.item_id
-            WHERE rentals.user_id = ?
-            ORDER BY rentals.rental_date ASC
-        `, [req.session.userID], (err, results) => {
-            if (err) {
-
-                console.log(err);
-                return;
-            }
-console.log('User ID:', req.session.userID);
-
-            // Ellenőrizd, hogy a 'results' tartalmaz adatokat
-            console.log(results); // Ez segít megérteni, hogy miért nem jelennek meg az adatok
-
-            // Az összes kölcsönzés dátumát formázni
-            results.forEach(item => {
-                item.rental_date = moment(item.rental_date).format('YYYY.MM.DD.');
-                if (item.return_date) {
-                    item.return_date = moment(item.return_date).format('YYYY.MM.DD.');
-                }
-            });
-
-            // EJS sablon renderelése
-            ejs.renderFile('./views/sajatkolcsonzo.ejs', { session: req.session, results }, (err, html) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-
-                // Üzenet törlése
-                req.session.msg = '';
-                res.send(html);
-            });
-        });
-
-        return;
-    }
-
-    // Ha a felhasználó nincs bejelentkezve, átirányítjuk a főoldalra
-    res.redirect('/');
-});
 
 
 
