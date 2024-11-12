d30fdd3a38ae9acbf051bba232dfb177c962ef82
dzsofri <beretizsofia@turr.hu>
2024-11-12 08:28:40 +0100

    return esetén item update

--- modules/kolcsonzes.js
+++ modules/kolcsonzes.js
@@ -4,7 +4,6 @@ const db = require('./database');
 
 
 
-// Example of route definition
 router.post('/return/:id', async (req, res) => {
     const rentalId = req.params.id; // Capture the rental_id from the URL
     const currentDate = new Date(); // Get the current date
@@ -21,13 +20,27 @@ router.post('/return/:id', async (req, res) => {
                     return res.status(500).send('Error updating the rental item');
                 }
 
-                // After the update, you can return a response or render a template
-                res.send('Item returned successfully!');
+                // Assuming rentals table has a reference to the item in item_id
+                db.query(`
+                    UPDATE items 
+                    SET available = 1  -- Set available to true (or 1, depending on your schema)
+                    WHERE item_id = (
+                        SELECT item_id FROM rentals WHERE rental_id = ?
+                    )`, [rentalId], (err, results) => {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500).send('Error updating item availability');
+                        }
+
+                        // After the updates, send a success response
+                        res.send('Item returned and marked as available!');
+                    });
             });
     } catch (error) {
         console.log(error);
         return res.status(500).send('Server error');
     }
 });
+
 
 module.exports = router;
